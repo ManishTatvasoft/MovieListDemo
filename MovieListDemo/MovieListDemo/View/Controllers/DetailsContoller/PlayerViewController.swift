d248b9d95feb83cfdf6c461142dd13f7e3d3ccae
Manish Chauhan <manish.chauhan@tatvasoft.com>
2022-06-10 12:42:43 +0530

    set video player view to safe area layout guide.

--- MovieListDemo/MovieListDemo/View/Controllers/DetailsContoller/PlayerViewController.swift
+++ MovieListDemo/MovieListDemo/View/Controllers/DetailsContoller/PlayerViewController.swift
@@ -30,19 +30,39 @@ class PlayerViewController: UIViewController {
         
         if #available(iOS 11.0, *){
             let videoPlayerView = WKWebView()
-            videoPlayerView.frame = self.view.bounds
+            videoPlayerView.translatesAutoresizingMaskIntoConstraints = false
             self.view.addSubview(videoPlayerView)
+            setupView(videoPlayerView)
             videoPlayerView.configuration.allowsInlineMediaPlayback = true
             videoPlayerView.load(youtubeRequest)
         }else{
             let videoPlayerView = UIWebView()
-            videoPlayerView.frame = self.view.bounds
+            videoPlayerView.translatesAutoresizingMaskIntoConstraints = false
             self.view.addSubview(videoPlayerView)
+            setupView(videoPlayerView)
             videoPlayerView.allowsInlineMediaPlayback = true
             videoPlayerView.loadRequest(youtubeRequest)
         }
         
-        
+        func setupView(_ webView: UIView){
+            if #available(iOS 11, *) {
+              let guide = view.safeAreaLayoutGuide
+                NSLayoutConstraint.activate([
+                    webView.topAnchor.constraint(equalToSystemSpacingBelow: guide.topAnchor, multiplier: 1.0),
+                    guide.bottomAnchor.constraint(equalToSystemSpacingBelow: webView.bottomAnchor, multiplier: 1.0),
+                    webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                    webView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+                ])
+            } else {
+               let standardSpacing: CGFloat = 8.0
+                NSLayoutConstraint.activate([
+                    webView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: standardSpacing),
+                    bottomLayoutGuide.topAnchor.constraint(equalTo: webView.bottomAnchor, constant: standardSpacing),
+                    webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                    webView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+                ])
+            }
+        }
 
     }
 
